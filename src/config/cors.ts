6bbd0b2603592df37f1ea4b8ba71c2ed3cec0c58
JamesSmurf1 <parththebest123@gmail.com>
2024-12-21 12:58:44 +0800

    update 1.0.1

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,15 +3,16 @@ import { cors } from "hono/cors";
 
 config();
 
-const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
-  ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:4000", "*"];
+// const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
+//   ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
+//   : ["http://localhost:4000", "*"];
 
 const corsConfig = cors({
   allowMethods: ["GET"],
   maxAge: 600,
   credentials: true,
-  origin: allowedOrigins,
+  // origin: allowedOrigins,
+  origin: '*',
 });
 
 export default corsConfig;
